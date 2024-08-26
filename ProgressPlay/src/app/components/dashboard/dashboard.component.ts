7d4c89d625b7f288d799f31b1502e9ff8546d730
samrath-ahluwalia <samrathahluwalia999@gmail.com>
2024-08-26 20:36:37 +0530

    Added Additional Components

--- ProgressPlay/src/app/components/dashboard/dashboard.component.ts
+++ ProgressPlay/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  allTodoLists: any = [];
+  allTodoItems: any = [];
+  selectedList: any = "selectedList";
+  dateCondition: boolean = false;
+  emptyLists: any = []
 }
