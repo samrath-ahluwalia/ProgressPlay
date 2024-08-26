0af854de528aac10be7c7c63a7d3936ca7dc6e26
samrath-ahluwalia <samrathahluwalia999@gmail.com>
2024-08-26 19:43:14 +0530

    Added Components and Routing

--- ProgressPlay/src/app/app.routes.ts
+++ ProgressPlay/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { EntryComponent } from './components/entry/entry.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AboutComponent } from './components/about/about.component';
+import { profileEnd } from 'console';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RewardsComponent } from './components/rewards/rewards.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: EntryComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'signup', component: SignupComponent},
+    {path: 'homepage', component: HomepageComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'rewards', component: RewardsComponent},
+    {path: '**', component: PagenotfoundComponent},
+];
